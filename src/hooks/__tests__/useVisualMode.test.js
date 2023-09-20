83a9b356b9232cbaa86677e8ef27b191149a7a44
kazvee <kayvee123@gmail.com>
2023-09-20 13:35:03 -0600

    Build useVisualMode with Test Driven Development & ensure test passes

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,3 +1,10 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from '@testing-library/react-hooks';
+import useVisualMode from 'hooks/useVisualMode';
 
-import useVisualMode from "hooks/useVisualMode";
+const FIRST = 'FIRST';
+
+test('useVisualMode initializes with default value', () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const useVisualMode = (initialMode) => {
+  return { mode: initialMode };
+};
+
+export default useVisualMode;
