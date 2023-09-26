f516aaec773967e680ffbc73aa96fd0b6ad2a28b
kazvee <kayvee123@gmail.com>
2023-09-26 16:53:08 -0600

    Add tests for appointment form input validation on submit

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -25,6 +25,11 @@ export default function Form(props) {
       return;
     }
 
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
     setError("");
     props.onSave(name, interviewer);
   }
