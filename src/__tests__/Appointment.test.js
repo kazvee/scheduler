f5e3a0fd28d811b3f9e43e3bca66868b7c089c0a
kazvee <kayvee123@gmail.com>
2023-09-27 09:03:10 -0600

    Implement mock axios module to enable API request testing

--- src/__tests__/Appointment.test.js
+++ src/__tests__/Appointment.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import Appointment from 'components/Appointment';
+import { render, waitForElement, fireEvent } from '@testing-library/react';
+import Application from 'components/Application';
 
 describe('Appointment', () => {
-  it('renders without crashing', () => {
-    render(<Appointment />);
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
