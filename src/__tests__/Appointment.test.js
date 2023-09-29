74671c7b2db375599a3ac7741c00c5b7d0693d01
kazvee <kayvee123@gmail.com>
2023-09-29 07:44:01 -0600

    Refactor test to use sync/await syntax instead of raw Promise syntax

--- src/__tests__/Appointment.test.js
+++ src/__tests__/Appointment.test.js
@@ -3,12 +3,13 @@ import { render, waitForElement, fireEvent } from '@testing-library/react';
 import Application from 'components/Application';
 
 describe('Appointment', () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
   
-    return waitForElement(() => getByText("Monday")).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
