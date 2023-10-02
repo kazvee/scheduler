35d7bb487aa191b15d6016ce6fd069dd503b83c9
kazvee <kayvee123@gmail.com>
2023-10-02 13:20:14 -0600

    Add integration test for interview appointment booking

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, waitForElement, fireEvent } from '@testing-library/react';
+import { render, waitForElement, fireEvent, prettyDOM, getByText, getByPlaceholderText, getByAltText, getAllByTestId } from '@testing-library/react';
 import Application from 'components/Application';
 
-describe('Appointment', () => {
+describe('Application', () => {
   it('defaults to Monday and changes the schedule when a new day is selected', async () => {
     const { getByText } = render(<Application />);
 
@@ -11,5 +11,25 @@ describe('Appointment', () => {
     fireEvent.click(getByText('Tuesday'));
 
     expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
+
+  it('loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, 'Add'));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' },
+    });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+
+    fireEvent.click(getByText(appointment, 'Save'));
+
+    console.log(prettyDOM(appointment));
   });
 });
