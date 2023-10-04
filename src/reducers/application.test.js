08b2cbde0b506ab42fb4177f6b105614ac9737f8
kazvee <kayvee123@gmail.com>
2023-10-04 11:33:49 -0600

    Refactor Application Reducer test

--- src/reducers/application.test.js
+++ src/reducers/application.test.js
@@ -1,7 +1,14 @@
-import reducer from "reducers/application";
+import reducer from 'reducers/application';
 
-describe("Application Reducer", () => {
-  it("thows an error with an unsupported type", () => {
-    expect(() => reducer({}, { type: null })).toThrow();
+describe('Application Reducer', () => {
+  
+  // it("thows an error with an unsupported type", () => {
+  //   expect(() => reducer({}, { type: null })).toThrow();
+  // });
+
+  it('thows an error with an unsupported type', () => {
+    expect(() => reducer({}, { type: null })).toThrowError(
+      /tried to reduce with unsupported action type/i
+    );
   });
 });
