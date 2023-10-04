3e06a6ffc2400f23a4cc483fe31a681dce6bf641
kazvee <kayvee123@gmail.com>
2023-10-04 15:17:08 -0600

    Code cleanup: Remove unused test code & comments with learning notes

--- cypress/integration/1-navigation.spec.js
+++ cypress/integration/1-navigation.spec.js
@@ -5,17 +5,6 @@ describe('Navigation', () => {
 
   it('navigates to Tuesday', () => {
     cy.visit('/');
-
-    // Works:
-    // cy.get("li").contains("Tuesday").click()
-    // cy.contains("li", "Tuesday").should("have.css", "background-color", "rgb(242, 242, 242)")
-
-    // Refactored to use a single command chain that finds the list item, clicks it and checks it for the correct background colour:
-    // cy.contains('li', 'Tuesday')
-    //   .click()
-    //   .should('have.css', 'background-color', 'rgb(242, 242, 242)');
-
-    // Refactored to align with best practice and use the data-testid attribute and 'selected' css class to make the test more durable:
     cy.contains('[data-testid=day]', 'Tuesday')
       .click()
       .should('have.class', 'day-list__item--selected');
